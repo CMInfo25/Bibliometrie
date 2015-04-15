b7bb1f6202504dee8d04266995d096d47cbee330
Romain Mekarni <romain.mekarni@gmail.com>
2015-04-15 16:45:27 +0200

    edit base OK

--- Base.java
+++ Base.java
@@ -1,14 +1,47 @@
 public class Base
 {
     static String ip = "172.20.128.64";
-    static String user = "rmekarni";
-    static String pass = "rmekarni";
-    static String base = "rmekarni_BD";
+    static String user = "beaumann";
+    static String pass = "beaumann";
+    static String base = "beaumann_BD";
 
     static int co = BD.ouvrirConnexion(ip, base, user, pass);
 
-    static void getPublications(String auteur)
+    static void addAuteur(String nom, String prenom)
     {
+      BD.executerUpdate(co, "INSERT INTO AUTEUR (auNom, auPrenom) VALUES ('" + nom + "', '" + prenom + "')");
+    }
+  
+    static void addPublication(String titre, String type, String recueil, int annee)
+    {
+      BD.executerUpdate(co, "INSERT INTO PUBLICATION (pubTitre, pubType, pubRecueil, pubAnnee) VALUES ('" + titre + "', '" + type + "', '" + recueil + "', '" + String.valueOf(annee) + "')");
+    }
+  
+    static void addPubliAuteur(int auteur, int publication)
+    {
+      BD.executerUpdate(co, "INSERT INTO PUBLI_AUTEUR VALUES ('" + auteur + "', '" + publication + "')");
+    }
 
+    static void getPublications(int auteur)
+    {
+      int res = BD.executerSelect(co, "SELECT paPublication FROM PUBLI_AUTEUR WHERE PUBLI_AUTEUR.paAuteur = " + auteur);
+      while (BD.suivant(res))
+      {
+        Ecran.afficher("Publication ", BD.attributInt(res, "paPublication"), "\n");
+        getPublication(BD.attributInt(res, "paPublication"));
+      }
+    }
+  
+    static void getPublication(int id)
+    {
+      int res = BD.executerSelect(co, "SELECT * FROM PUBLICATION WHERE pubID = " + id);
+      while (BD.suivant(res))
+      {
+        Ecran.afficher("Publication ", BD.attributInt(res, "pubID"), "\n");
+        Ecran.afficher("Titre : ", BD.attributString(res, "pubTitre"), "\n");
+        Ecran.afficher("Recueil : ", BD.attributString(res, "pubRecueil"), "\n");
+        Ecran.afficher("Type : ", BD.attributString(res, "pubType"), "\n");
+        Ecran.afficher("Annee : ", BD.attributInt(res, "pubAnnee"), "\n");
+      }
     }
 }
--- Bibliometrie.java
+++ Bibliometrie.java
@@ -1,10 +1,15 @@
 public class Bibliometrie
 {
-    static run = true;
+    static boolean run = true;
 
     static void printCommands()
     {
-        Ecran.afficher("Quitter : q\n");
+      Ecran.afficher("Ajouter un auteur : a\n");
+      Ecran.afficher("Ajouter une publication : p\n");
+      Ecran.afficher("Ajouter une publication à un auteur : ap\n");
+      Ecran.afficher("Afficher les publications d'un auteur : apa");
+      Ecran.afficher("Afficher l'aide : h\n");
+      Ecran.afficher("Quitter : q\n");
     }
 
     public static void main(String[] argv)
@@ -14,7 +19,43 @@ public class Bibliometrie
         while (run)
         {
             input = Clavier.saisirString();
-            if (input != ":q")
+            if (input.equals("a"))
+            {
+              Ecran.afficher("\nNom : ");
+              String nom = Clavier.saisirString();
+              Ecran.afficher("\nPrenom : ");
+              String prenom = Clavier.saisirString();
+              Base.addAuteur(nom, prenom);
+            }
+            if (input.equals("p"))
+            {
+              Ecran.afficher("\nTitre : ");
+              String titre = Clavier.saisirString();
+              Ecran.afficher("\nType ('conference' ou 'journal') : ");
+              String type = Clavier.saisirString();
+              Ecran.afficher("\nRecueil : ");
+              String recueil = Clavier.saisirString();
+              Ecran.afficher("\nAnnee : ");
+              int annee = Clavier.saisirInt();
+              Base.addPublication(titre, type, recueil, annee);
+            }
+            if (input.equals("ap"))
+            {
+              Ecran.afficher("\nid Auteur : ");
+              int auteur = Clavier.saisirInt();
+              Ecran.afficher("\nid Publication : ");
+              int publication = Clavier.saisirInt();
+              Base.addPubliAuteur(auteur, publication);
+            }
+            if (input.equals("apa"))
+            {
+              Ecran.afficher("\nid Auteur : ");
+              int auteur = Clavier.saisirInt();
+              Base.getPublications(auteur);
+            }
+            if (input.equals("h"))
+                printCommands();
+            if (input.equals("q"))
                 run = false;
         }
     }
